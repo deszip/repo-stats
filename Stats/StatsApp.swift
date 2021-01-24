f7435d2b839bc624135e767c8afbe00dc79d27a0
Deszip <igor.asharenkov@techery.io>
2021-01-24 18:20:10 +0200

    Initial Commit

---
+++ Stats/StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatsApp.swift
+//  Stats
+//
+//  Created by Deszip on 24.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
