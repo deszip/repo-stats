320a59f20a554db8c47d00af68a40d82e1b333b4
Deszip <deszip@DuckBox.local>
2024-04-16 20:50:05 +0300

    git clone auth

--- StatsCore/STGitToolkit.m
+++ StatsCore/STGitToolkit.m
@@ -26,6 +26,25 @@
 /// git clone --single-branch --branch [branch_name] [repo_url]
 - (void)cloneRepo:(NSString *)repoPath branch:(NSString *)branch {
     NSTask *cloneTask = [NSTask new];
+
+    NSDictionary *environmentDict = [[NSProcessInfo processInfo] environment];
+    // Environment variables needed for password based authentication
+    NSString *askPassPath = [NSBundle pathForResource:@"Stats"
+                  ofType:@""
+                  inDirectory:[[NSBundle mainBundle] bundlePath]];
+    NSMutableDictionary *env = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                             @"NONE", @"DISPLAY",                           askPassPath, @"SSH_ASKPASS",
+                             @"deszip",@"AUTH_USERNAME",
+                             @"github.com",@"AUTH_HOSTNAME",
+                             nil];
+     
+    // Environment variable needed for key based authentication
+//    [env setObject:[environmentDict objectForKey:@"SSH_AUTH_SOCK"] forKey:@"SSH_AUTH_SOCK"];
+     
+    // Setting the task's environment
+//    [cloneTask setEnvironment:env];
+//    [cloneTask setEnvironment:environmentDict];
+    
     cloneTask.launchPath = @"/usr/bin/git";
     cloneTask.arguments = @[@"clone", @"--single-branch", @"--branch", branch, repoPath, self.workingDirectory.path];
     [cloneTask launch];
